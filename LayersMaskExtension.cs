e11d7d02da1aa52354039a48e72b138d825b3f18
yjplapian <yajupalapian@outlook.com>
2023-09-19 16:31:32 +0200

    Updated to match TitleCase conventions

--- LayersMaskExtension.cs
+++ LayersMaskExtension.cs
@@ -2,20 +2,24 @@
 {
     public static class LayersMaskExtension
     {
-        public static LayersMask SetLayer(LayersMask LayersMask, int[] index)
+        /// <summary>
+        /// Sets a mutlitude layers of the edited LayersMask
+        /// </summary>
+        /// <param name="LayersMask"> The LayerMask being edited </param>
+        /// <param name="Index"> The targeted layers </param>
+        /// <returns> A LayersMask with the specified layers checked </returns>
+        public static LayersMask SetLayer(LayersMask LayersMask, int[] Index)
         {
-            foreach(var value in index)
-            {
+            foreach(var value in Index)
                 LayersMask.Mask += (uint)value;
-            }
 
             return LayersMask;
         }
 
-        public static LayersMask SetLayer(LayersMask layersMask, int index)
+        public static LayersMask SetLayer(LayersMask LayersMask, int Index)
         {
-            layersMask.Mask = (uint)index;
-            return layersMask;
+            LayersMask.Mask = (uint)Index;
+            return LayersMask;
         }
     }
 }
