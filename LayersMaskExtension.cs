07e30fde571a4bd6dcaff3c42b07fbdcd4eeb6c5
yjplapian <yajupalapian@outlook.com>
2023-09-19 16:21:09 +0200

    Extension on LayersMask :: Set layers by index

---
+++ LayersMaskExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace FlaxEngine
+{
+    public static class LayersMaskExtension
+    {
+        public static LayersMask SetLayer(LayersMask LayersMask, int[] index)
+        {
+            foreach(var value in index)
+            {
+                LayersMask.Mask += (uint)value;
+            }
+
+            return LayersMask;
+        }
+
+        public static LayersMask SetLayer(LayersMask layersMask, int index)
+        {
+            layersMask.Mask = (uint)index;
+            return layersMask;
+        }
+    }
+}
